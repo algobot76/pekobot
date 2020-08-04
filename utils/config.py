0b6c7493033a1c6126197bbce77cf62845871394
Kaitian Xie <xkaitian@gmail.com>
2020-08-04 01:54:13 -0700

    Support ignored channels (#27)

--- utils/config.py
+++ utils/config.py
@@ -7,10 +7,12 @@ def load_config(config_path="pekobot-config.yaml"):
 
     token = data["discord_token"]
     cogs = data["cogs"]
+    ignored_channels = data["ignored_channels"]
     pixiv_username = data["pixiv"]["username"]
     pixiv_password = data["pixiv"]["password"]
 
     return dict(discord_token=token,
+                ignored_channels=ignored_channels,
                 pixiv_username=pixiv_username,
                 pixiv_password=pixiv_password,
                 cogs=cogs)
