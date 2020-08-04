741e2131f87181f8080caea84efe5e6394504832
Kaitian Xie <xkaitian@gmail.com>
2020-08-04 06:59:12 -0700

    Add nyb

--- cogs/peko.py
+++ cogs/peko.py
@@ -1,4 +1,13 @@
+import os
+
+import discord
 from discord.ext import commands
+
+NYB_TEXT = '''
+正在播放：New Year Burst
+──●━━━━ 1:05/1:30
+⇆ ㅤ◁ ㅤㅤ❚❚ ㅤㅤ▷ ㅤ↻
+'''
 
 
 class Peko(commands.Cog):
@@ -17,6 +26,13 @@ class Peko(commands.Cog):
             await ctx.send("老娘不认识的指令呢～～")
             return
         raise error
+
+    @commands.Cog.listener()
+    async def on_message(self, msg):
+        if "春黑" in msg.content:
+            await msg.channel.send(
+                file=discord.File(os.path.join("cogs", "data", "nyb.gif")))
+            await msg.channel.send(NYB_TEXT)
 
 
 def setup(bot):
