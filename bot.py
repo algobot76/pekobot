6ae6d5c1573aaf98c64bd985602b47f2a95678ff
Kaitian Xie <xkaitian@gmail.com>
2020-08-04 12:47:42 -0700

    Disable pcrclanbattles for now

--- bot.py
+++ bot.py
@@ -6,22 +6,18 @@ import brotli
 from discord.ext import commands
 
 from utils import config
-from utils import db
 
 
 class Bot(commands.Bot):
     def __init__(self, **kwargs):
         super().__init__(command_prefix=kwargs.pop("command_prefix"))
 
-        self.db_conn = kwargs.pop("db_conn")
-
 
 conf = config.load_config()
 
 
 async def run():
-    conn = db.create_connection("pekobot.db")
-    bot = Bot(command_prefix=("!", "！"), db_conn=conn)
+    bot = Bot(command_prefix=("!", "！"))
     for cog in conf["cogs"]:
         bot.load_extension(cog)
 
@@ -42,7 +38,6 @@ async def run():
     try:
         await bot.start(conf['discord_token'])
     except KeyboardInterrupt:
-        conn.close()
         await bot.logout()
 
 
