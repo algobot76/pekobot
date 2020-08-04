22996c850f14b2e01463cea0ec61c8dc831ebf01
Kaitian Xie <xkaitian@gmail.com>
2020-08-04 01:30:35 -0700

    Greet new members (#26)

--- cogs/peko.py
+++ cogs/peko.py
@@ -6,6 +6,12 @@ class Peko(commands.Cog):
         self.bot = bot
 
     @commands.Cog.listener()
+    async def on_member_join(self, member):
+        channel = member.guild.system_channel
+        if channel is not None:
+            await channel.send('等你好久了 {0.mention}.'.format(member))
+
+    @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
         if isinstance(error, commands.CommandNotFound):
             await ctx.send("老娘不认识的指令呢～～")
