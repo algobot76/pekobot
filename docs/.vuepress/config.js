5b7be4c319dc66f4a5cfdac5e183365ae3abf905
Kaitian Xie <xkaitian@gmail.com>
2020-08-03 20:51:56 -0700

    Add deploy script

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -21,6 +21,8 @@ module.exports = {
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
   ],
 
+  base: "/pekobot/",
+
   /**
    * Theme configuration, here is the default theme configuration for VuePress.
    *
