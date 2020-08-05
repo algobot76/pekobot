3eb0f52c656ee4fe33a2f329124c0e5490c3bfc6
Kaitian Xie <xkaitian@gmail.com>
2020-08-05 11:51:33 -0700

    Update setu cog

--- pekobot/cogs/setu.py
+++ pekobot/cogs/setu.py
@@ -1,27 +1,29 @@
+import logging
 import os
 import random
 
-import discord
+from discord import File
 from discord.ext import commands
+
+from pekobot.bot import Bot
+from pekobot.utils import checks
+
+logger = logging.getLogger(__name__)
 
 
 class Setu(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: Bot):
         self.bot = bot
         self.setu_path = os.path.join("images", "setu")
 
-    @commands.command(name="setu", aliases=("Setu", "色图", "涩图"))
-    async def send_setu(self, ctx):
-        if ctx.channel.is_nsfw():
-            setu_images = os.listdir(self.setu_path)
-            num = random.randint(0, len(setu_images))
-            if num == 0:
-                await ctx.send("我已经被榨干了，呜～～")
-            else:
-                await ctx.send(file=discord.File(
-                    os.path.join(self.setu_path, setu_images[num - 1])))
-        else:
-            await ctx.send("你想要啥呢？变态ヽ(`⌒´メ)ノ")
+    @commands.command(name="setu", aliases=("色图", "涩图"))
+    @checks.is_nsfw()
+    async def send_setu(self, ctx: commands.Context):
+        author = ctx.author
+        logger.info(f"{author} is requesting a setu.")
+        setu_images = os.listdir(self.setu_path)
+        selected_image = random.choice(setu_images)
+        await ctx.send(file=File(os.path.join(self.setu_path, selected_image)))
 
 
 def setup(bot):
