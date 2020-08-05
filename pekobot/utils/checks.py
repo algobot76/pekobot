a322b5dc15742806cf6855ec136e9e042c9c6bd0
Kaitian Xie <xkaitian@gmail.com>
2020-08-05 11:52:47 -0700

    Update setu cog (#50)

--- pekobot/utils/checks.py
+++ pekobot/utils/checks.py
@@ -1,14 +1,25 @@
-import discord.ext
 from discord.ext import commands
 
 
 def is_admin():
-    async def predicate(ctx: discord.ext.commands.Context):
+    async def predicate(ctx: commands.Context):
         ch = ctx.channel
         permissions = ch.permissions_for(ctx.author)
 
         if not permissions.administrator:
             await ctx.send("此功能只对管理员开放")
+            return False
+        return True
+
+    return commands.check(predicate)
+
+
+def is_nsfw():
+    async def predicate(ctx: commands.Context):
+        ch = ctx.channel
+
+        if not ch.is_nsfw():
+            await ctx.send("你在想啥呢？变态ヽ(`⌒´メ)ノ")
             return False
         return True
 
