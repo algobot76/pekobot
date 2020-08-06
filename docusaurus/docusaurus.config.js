7626511c4fd630b16686547c2bd02f67120d8e87
Kaitian Xie <xkaitian@gmail.com>
2020-08-05 21:08:53 -0700

    Add docusaurus

---
+++ docusaurus/docusaurus.config.js
@@ -0,0 +1,102 @@
+module.exports = {
+  title: 'Pekobot',
+  tagline: '公主连结R玩家专用Discord Bot',
+  url: 'https://marchen-kingdom.github.io',
+  baseUrl: '/pekobot/',
+  onBrokenLinks: 'throw',
+  favicon: 'img/favicon.ico',
+  organizationName: 'marchen-kingdom', // Usually your GitHub org/user name.
+  projectName: 'pekobot', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'Pekobot',
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: '文档',
+          position: 'left',
+        },
+        {to: 'blog', label: '博客', position: 'left'},
+        {
+          href: 'https://github.com/marchen-kingdom/pekobot',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Style Guide',
+              to: 'docs/',
+            },
+            {
+              label: 'Second Doc',
+              to: 'docs/doc2/',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          // It is recommended to set document id as docs home page (`docs/` path).
+          homePageId: 'doc1',
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
