b70e703fdcdb171b2b22b37d071a74eb68553e3d
Kaitian Xie <xkaitian@gmail.com>
2020-08-08 16:02:15 -0700

    Code cleanup

--- pekobot/cogs/pcrclanbattles.py
+++ pekobot/cogs/pcrclanbattles.py
@@ -9,6 +9,8 @@ from pekobot.utils import db
 
 logger = logging.getLogger(__name__)
 
+CLAN_MEMBER_TABLE = "clan_member"
+
 
 class PCRClanBattles(commands.Cog, name="PCR公会战插件"):
     """The PCR clan battles cog.
@@ -16,9 +18,6 @@ class PCRClanBattles(commands.Cog, name="PCR公会战插件"):
     Attributes:
         bot: A pekobot instance.
     """
-
-    CLAN_MEMBER_TABLE = "clan_member"
-
     def __init__(self, bot: Pekobot):
         self.bot = bot
 
@@ -30,20 +29,20 @@ class PCRClanBattles(commands.Cog, name="PCR公会战插件"):
 
         logger.info("Creating a clan for the guild %s.", ctx.guild)
         with sqlite3.connect(self._get_db_name(ctx)) as conn:
-            if not db.table_exists(conn, self.CLAN_MEMBER_TABLE):
+            if not db.table_exists(conn, CLAN_MEMBER_TABLE):
                 cursor = conn.cursor()
                 create_table = f'''
-                CREATE TABLE {self.CLAN_MEMBER_TABLE} (
+                CREATE TABLE {CLAN_MEMBER_TABLE} (
                     member_id INTEGER PRIMARY KEY,
                     member_name TEXT,
                     member_nick TEXT
                 )
                 '''
                 cursor.execute(create_table)
-                logger.info("The clan has been created.")
+                logger.info("The clan %s has been created.", ctx.guild)
                 await ctx.send("建会成功")
             else:
-                logger.warning("The clan already exists.")
+                logger.warning("The clan %s already exists.", ctx.guild)
                 await ctx.send("公会已存在")
 
     @commands.command(name="join-clan", aliases=("入会", ))
@@ -51,10 +50,11 @@ class PCRClanBattles(commands.Cog, name="PCR公会战插件"):
     async def join_clan(self, ctx: commands.Context):
         """加入公会。"""
 
-        logger.info("%s is trying to join the clan.", ctx.author)
+        logger.info("%s (%s) is joining the clan.", ctx.author, ctx.guild)
         with sqlite3.connect(self._get_db_name(ctx)) as conn:
-            if not db.table_exists(conn, self.CLAN_MEMBER_TABLE):
-                logger.error("The clan has not been created yet.")
+            if not db.table_exists(conn, CLAN_MEMBER_TABLE):
+                logger.error("The clan %s has not been created yet.",
+                             ctx.guild)
                 await ctx.send("公会尚未建立")
             else:
                 cursor = conn.cursor()
@@ -65,7 +65,8 @@ class PCRClanBattles(commands.Cog, name="PCR公会战插件"):
                 '''
                 cursor.execute(check_member)
                 if cursor.fetchone()[0] != 0:
-                    logger.warning("%s is already in the clan.", author)
+                    logger.warning("%s is already in the clan %s.", author,
+                                   ctx.guild)
                     await ctx.send("你已是公会成员")
                     return
 
@@ -80,10 +81,10 @@ class PCRClanBattles(commands.Cog, name="PCR公会战插件"):
                 '''
                 logger.info(
                     "Inserting (member_id: %d, member_name: '%s') into %s.",
-                    author.id, author, self.CLAN_MEMBER_TABLE)
+                    author.id, author, CLAN_MEMBER_TABLE)
                 cursor.execute(add_member)
                 conn.commit()
-                logger.info("%s has joined the clan.", author)
+                logger.info("%s has joined the clan %s.", author, ctx.guild)
                 await ctx.send("入会成功")
 
     @commands.command(name="list-members", aliases=("查看成员", ))
@@ -91,11 +92,13 @@ class PCRClanBattles(commands.Cog, name="PCR公会战插件"):
     async def list_members(self, ctx: commands.Context):
         """查看公会成员。"""
 
-        logger.info("%s wants to list all members of the clan.", ctx.author)
+        logger.info("%s (%s) wants to list all members of the clan.",
+                    ctx.author, ctx.guild)
         with sqlite3.connect(self._get_db_name(ctx)) as conn:
             cursor = conn.cursor()
-            if not db.table_exists(conn, self.CLAN_MEMBER_TABLE):
-                logger.error("The clan has not been created yet.")
+            if not db.table_exists(conn, CLAN_MEMBER_TABLE):
+                logger.error("The clan %s has not been created yet.",
+                             ctx.guild)
                 await ctx.send("公会尚未建立")
             else:
                 list_members = '''
@@ -123,8 +126,8 @@ class PCRClanBattles(commands.Cog, name="PCR公会战插件"):
             ctx: A command context
         """
 
-        guid_id = ctx.guild.id
-        return f"pcrclanbattles-{guid_id}.db"
+        guild_id = ctx.guild.id
+        return f"pcrclanbattles-{guild_id}.db"
 
 
 def setup(bot):
