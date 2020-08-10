1cb6079ece8cd099ecda6b34d93b693e909dd938
Kaitian Xie <xkaitian@gmail.com>
2020-08-09 19:35:52 -0700

    Add a custom help command (#103)

--- pekobot/bot.py
+++ pekobot/bot.py
@@ -1,6 +1,39 @@
 """An extension of commands.Bot"""
+import logging
+
 import aiohttp
 from discord.ext import commands
+
+logger = logging.getLogger(__name__)
+
+
+@commands.command(name="help", aliases=("帮助", ))
+async def help_(ctx: commands.Context):
+    """查看使用说明"""
+
+    logger.info("%s (%s) is asking for help.", ctx.author, ctx.guild)
+
+    cogs = [cog for _, cog in ctx.bot.cogs.items()]
+
+    manual = "使用说明\n"
+    manual += "=======\n"
+
+    for cog in cogs:
+        cog_name = cog.qualified_name
+        manual += f"{cog_name}：\n"
+
+        commands_ = cog.get_commands()
+        for command in commands_:
+            command_name = command.name
+            command_aliases = command.aliases
+            cmd = command_name
+            if command_aliases:
+                for alias in command_aliases:
+                    cmd += f"|{alias}"
+            command_description = f"![{cmd}]：{command.help}\n"
+            manual += command_description
+        manual += "-------\n"
+    await ctx.send(manual)
 
 
 class Pekobot(commands.Bot):
@@ -16,3 +49,6 @@ class Pekobot(commands.Bot):
 
         self.g = dict()  # inspired by flask.g
         self.g["pcr_db"] = kwargs.pop("pcr_db")
+
+        self.remove_command("help")
+        self.add_command(help_)
