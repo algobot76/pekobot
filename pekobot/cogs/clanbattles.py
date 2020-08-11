ed6053f253f634ec2282ff84829d2a93076b61ef
Kaitian Xie <xkaitian@gmail.com>
2020-08-10 23:10:45 -0700

    Add delete-clan-battle command

--- pekobot/cogs/clanbattles.py
+++ pekobot/cogs/clanbattles.py
@@ -43,6 +43,10 @@ WHERE date='%s'
 GET_ALL_CLAN_BATTLES = f"""
 SELECT date, name FROM {CLAN_BATTLE_TABLE};
 """
+DELETE_CLAN_BATTLE_BY_DATE = f"""
+DELETE FROM {CLAN_BATTLE_TABLE}
+WHERE date='%s';
+"""
 
 
 class ClanBattles(commands.Cog, name="公会战插件"):
@@ -262,6 +266,39 @@ class ClanBattles(commands.Cog, name="公会战插件"):
                     report += f"{date} ({name})\n"
                 report += "-------\n"
             await ctx.send(report)
+
+    @commands.command(name="delete-clan-battle", aliases=("删除会战", ))
+    @commands.guild_only()
+    @commands.has_permissions(administrator=True)
+    async def delete_clan_battle(self, ctx: commands.Context, date=""):
+        """删除公会战数据。"""
+
+        logger.info("%s (%s) is deleting a clan battle.", ctx.author,
+                    ctx.guild)
+        if not await self._check_date(ctx, date):
+            return
+
+        logger.info("The clan battle %s will be deleted.", date)
+        with sqlite3.connect(self._get_db_name(ctx)) as conn:
+            cursor = conn.cursor()
+            cursor.execute(COUNT_CLAN_BATTLE % date)
+            if cursor.fetchone()[0] == 0:
+                logger.warning("The clan battle %s does not exist.", date)
+                await ctx.send("此公会战不存在")
+            else:
+                logger.info("The clan battle %s is being deleted.", date)
+                cursor.execute(DELETE_CLAN_BATTLE_BY_DATE % date)
+                conn.commit()
+                with shelve.open(META_FILE_PATH, writeback=True) as s:
+                    guild_id = str(ctx.guild.id)
+                    try:
+                        curr_date = s[guild_id]["current_battle_date"]
+                        if curr_date == date:
+                            s[guild_id]["current_battle_date"] = ""
+                            s[guild_id]["current_battle_name"] = ""
+                    except KeyError:
+                        pass
+                await ctx.send("公会战已删除")
 
     @commands.command(name="export-data", aliases=("导出数据", ))
     @commands.guild_only()
